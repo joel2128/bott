bc3197565bceed9facfd6d9840c4a47984a90bd0
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-21 11:44:38 +0800

    Update ftf.ps1
    fix command path

--- ftf.ps1
+++ ftf.ps1
@@ -1,11 +1,31 @@
 # # FIRST THINGS FIRST
 
+
+
+# Define the URL of the file to download
+$url = "https://raw.githubusercontent.com/joel2128/bott/refs/heads/main/ftf.ps1"
+
+# Define the destination path in the %TEMP% directory
+$destination = "$env:TEMP\ftf.ps1"
+
+# Use Invoke-WebRequest to download the file
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+# Output the path to confirm where the file was saved
+Write-Output "File downloaded to: $destination"
+
+
+
 # Check if the script is running with elevated privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Define the path to %TEMP%\ftf.ps1
+    $scriptPath = "$env:TEMP\ftf.ps1"
+
     # Relaunch the script with elevated privileges
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
     exit
 }
+
 
 # Set the execution policy
 Set-ExecutionPolicy RemoteSigned -Force
