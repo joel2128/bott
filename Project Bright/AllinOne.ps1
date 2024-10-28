eebe142157f93c6537ca7f95a28eb8c126d91d8d
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-10-28 17:10:08 +0800

    Update AllinOne.ps1

--- Project Bright/AllinOne.ps1
+++ Project Bright/AllinOne.ps1
@@ -1,3 +1,5 @@
+# Script with 5 operations with progress bar
+
 # Discord webhook URL
 $webhookUrl = 'https://discord.com/api/webhooks/1297470837779333141/8AHSJu020L0KTuKxTcsMP5gaUQoy8M1IIX_1ts-DAsvj8748RNmEm0N9Xoxk-vy-_Gh-'
 
